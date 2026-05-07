da8d76f7f1a691481ba574af00a130a6ac84e8fd
kkouer <kkouer@live.cn>
2026-05-07 21:47:55 +0800

    add proximity S3 support

--- libraries/AP_Proximity/AP_Proximity_RPLidarA2.h
+++ libraries/AP_Proximity/AP_Proximity_RPLidarA2.h
@@ -143,7 +143,7 @@ private:
         _descriptor descriptor;
         _rpi_information information;
         _device_info device_info;
-        uint8_t forced_buffer_size[256]; // just so we read(...) efficiently
+        uint8_t forced_buffer_size[2048]; // just so we read(...) efficiently
     } _payload;
     static_assert(sizeof(_payload) >= 63, "Needed for parsing out reboot data");
 
@@ -154,6 +154,7 @@ private:
         A2M12,
         C1,
         S1,
+        S3,
     } model = Model::UNKNOWN;
 
     bool make_first_byte_in_payload(uint8_t desired_byte);
